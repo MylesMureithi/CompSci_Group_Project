fe4ce80bca746e8ec0fa261f22cc718f12632765
NigelAsante <nigelasante7@gmail.com>
2026-07-26 14:06:47 +0000

    Incorporated contingency plan to handle missing values

--- National Electricity Grid Network Analysis/Task 1.1.py
+++ National Electricity Grid Network Analysis/Task 1.1.py
@@ -2,13 +2,58 @@
 
 import pandas as pd
 import numpy as np
+import statistics as stat
 
 
-utilities = pd.read_csv('National Electricity Grid Network Analysis/data_files/utilities.csv') # utilities.csv
-substations = pd.read_csv('National Electricity Grid Network Analysis/data_files/substations.csv') # substations.csv
-lines = pd.read_csv('National Electricity Grid Network Analysis/data_files/lines.csv') # lines.csv
+utilities = pd.read_csv('utilities.csv') # utilities.csv
+substations = pd.read_csv('substations.csv') # substations.csv
+lines = pd.read_csv('lines.csv') # lines.csv
 
-#print(utilities.info())
+"""
+print(utilities.isna().sum())
+print()
+
+print(substations.isna().sum())
+print()
+
+print(lines.isna().sum())
+print()
+"""
+
+median_lat = stat.median(substations['Latitude'])
+print("Median Latitude:", median_lat)
+
+median_long = stat.median(substations['Longitude'])
+print("Median Longitude:", median_long)
+
+median_volt = stat.median(substations['Voltage (kV)'])
+print("Median Voltage:", median_volt)
+
+median_cap = stat.median(substations['Capacity (MVA)'])
+print("Median Capacity:", round(median_cap, 2))
+print()
+
+
+
+data = [
+    ['Latitude', 'Longitude', 'Voltage (kV)', 'Capacity (MVA)'],
+    [median_lat, median_long, median_volt, median_cap]
+]
+
+k = 0
+
+for i in data[0]:
+    for j, value in enumerate(substations[i]):
+        if pd.isna(value) == True:
+            substations.loc[j, i] = data[1][k]
+
+            k = k + 1
+
+            substations.to_csv("new_substations.csv", index=False)
+
+
+
+# with open()
 
 # Step 2: Handle missing values
     # Even though the generator produces clean data, treat this step seriously —
