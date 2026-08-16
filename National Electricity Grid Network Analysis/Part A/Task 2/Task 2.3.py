9d0b27db3976a73adf474560102eb2d6b565eccd
NigelAsante <nigelasante7@gmail.com>
2026-08-16 15:49:36 +0000

    Started Task 2.3

--- National Electricity Grid Network Analysis/Part A/Task 2/Task 2.3.py
+++ National Electricity Grid Network Analysis/Part A/Task 2/Task 2.3.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import statistics as stat
 
 utilities = pd.read_csv('National Electricity Grid Network Analysis/data_files/utilities.csv') # utilities.csv
 substations = pd.read_csv('National Electricity Grid Network Analysis/data_files/substations.csv') # substations.csv
@@ -8,51 +9,79 @@ lines = pd.read_csv('National Electricity Grid Network Analysis/data_files/lines
 # Load and capacity analysis
 # - Utility footprint by number of substations and lines, per region
 
-regions = substations['Region']
-line_sub_ids = list(lines['Source Substation ID'])
-sub_sub_ids = list(substations['Substation ID'])
 utility_ali = list(utilities['Alias'])
-
 line_utility_ids = list(lines['Utility ID'])
 
-dict1 = {}
+line_util_alias = {}
 
 for id in line_utility_ids:
     alias = utility_ali[id - 1]
     
-    if alias in dict1:
-        dict1[alias] += 1
+    if alias in line_util_alias:
+        line_util_alias[alias] += 1
     else:
-        dict1[alias] = 1
+        line_util_alias[alias] = 1
 
-print(dict1)
+print()
 
-"""for id in utility_ids:
-    if id in substationinregion:
-        utility_ali[substationinregion[utility_ids[id]]] = substationinregion[id] + 1
+utility_ali = list(utilities['Alias'])
+util_country = list(utilities['Country'])
+subs_country = list(substations['Country'])
+
+# Create a mapping from Country to Utility Alias
+country_to_alias = {}
+for i, country in enumerate(util_country):
+    country_to_alias[country] = utility_ali[i]
+
+subs_util_alias = {}
+
+for country in subs_country:
+    alias = country_to_alias.get(country, "Unknown")
+
+    if alias in subs_util_alias:
+        subs_util_alias[alias] += 1
     else:
-        substationinregion[id] = 1
+        subs_util_alias[alias] = 1
 
-print(substationinregion)"""
+#print(subs_util_alias) reveals 6 "Unknown" substations due to mismatching 'Country' names in Utilities.
 
-regionlines={}
-for value in line_sub_ids:
-    if value in sub_sub_ids:
-        place = sub_sub_ids.index(value)
-        region = regions[place]
-        if region in regionlines:
-            regionlines[region] = regionlines[region] + 1
-        else:
-            regionlines[region] = 1
+for util, lines, subs in zip(line_util_alias.keys(), line_util_alias.values(), subs_util_alias.values()):
+    print(f"Utility {util} has {subs} substations and {lines} lines.")
 
-
-"""for reg in substationinregion:
-    numberlines =regionlines.get(reg, 0)
-    print(f"Region {reg} has {substationinregion[reg]} substations and {numberlines} lines.")"""
-
+print()
 
 # - Identify substations operating close to rated capacity (upgrade candidates)
+voltage = list(substations['Voltage (kV)'])
+capacity = list(substations['Capacity (MVA)'])
+sub_names = list(substations['Name'])
+
+volt_cap_ratio = {}
+
+for name, volt, cap in zip(sub_names, voltage, capacity):
+    # We prevent division by zero just in case
+    if volt > 0:
+        capacity_ratio = cap / volt
+    else:
+        capacity_ratio = 0.0
+        
+    volt_cap_ratio[name] = round(capacity_ratio, 3)
+
+
+cap_mean = stat.mean(volt_cap_ratio.values())
+cap_sdev = stat.stdev(volt_cap_ratio.values())
+
+for key, value in zip(volt_cap_ratio.keys(), volt_cap_ratio.values()):
+    if value > (cap_mean + cap_sdev):
+        print(f"{key} is close to max capacity!")
+    else:
+        print(f"{key} is less than max capacity.")
+
+print()
+
+
 # - Identify underserved regions with growth potential
+
+
 # - Calculate a simple technical-loss proxy using line length and voltage
 # - Analyse utility asset age using Commissioning Year
  
